405553e881870c6575eeccb315968ceacc543b85
lucas <lucas@singular.inf.br>
2022-09-27 22:21:39 -0300

    registro admin, correção match_card

--- backend/game/admin.py
+++ backend/game/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
-# Register your models here.
+from backend.game.models import *
+
+admin.site.register(BoardEntity)
+admin.site.register(Board)
+admin.site.register(Card)
+admin.site.register(MatchCard)
+admin.site.register(MatchUser)
+admin.site.register(Match)
+admin.site.register(Position)
+admin.site.register(Room)
+admin.site.register(CardThrow)
+admin.site.register(Throw)
+admin.site.register(User)
--- backend/game/models/match_card.py
+++ backend/game/models/match_card.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class MatchCard(models.Model):
     match = models.ForeignKey('Match', on_delete=models.DO_NOTHING)
-    card = models.ForeignKey('User', on_delete=models.DO_NOTHING)
+    card = models.ForeignKey('Card', on_delete=models.DO_NOTHING)
