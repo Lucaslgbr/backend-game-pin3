0245ef5ae4461078dfd5b3adc3ca86d489b562c4
lucas <lucas@singular.inf.br>
2022-11-26 01:50:35 -0300

    correção room default status

---
+++ backend/game/api/v1/serializers/__init__.py
@@ -0,0 +1,5 @@
+from .room_serializer import RoomSerializer
+from .board_serializer import BoardSerializer
+from .match_serializer import MatchSerializer
+from .position_serializer import PositionSerializer
+from .user_serializer import UserSerializer
--- backend/game/models/room.py
+++ backend/game/models/room.py
@@ -5,7 +5,7 @@ from backend.game.enums.room_status import RoomStatus
 class Room(models.Model):
     name = models.CharField(max_length=255)
     password = models.CharField(max_length=255, blank=True, null=True)
-    status = models.IntegerField(choices=RoomStatus.choices, default=RoomStatus)
+    status = models.IntegerField(choices=RoomStatus.choices, default=RoomStatus.WAITING_FOR_PLAYERS)
     max_players = models.PositiveIntegerField()
     board = models.ForeignKey('Board', on_delete=models.DO_NOTHING)
     owner = models.ForeignKey('User', on_delete=models.DO_NOTHING)
