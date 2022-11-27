47d26349153f3080f8ad390d10708b8c54d172f1
lucas <lucas@singular.inf.br>
2022-11-27 19:48:56 -0300

    DRF settings e put room

--- backend/game/api/v1/viewsets/room_viewset.py
+++ backend/game/api/v1/viewsets/room_viewset.py
@@ -1,8 +1,17 @@
 from rest_framework.viewsets import ModelViewSet
 from backend.game.api.v1.serializers.room_serializer import RoomSerializer
-from backend.game.models import Room
+from backend.game.models import Room, User
 
 
 class  RoomViewset(ModelViewSet):
     queryset = Room.objects.all()
     serializer_class=RoomSerializer
+
+    def update(self, request, *args, **kwargs):
+        users = request.data.get('users')
+        request.data.pop('users')
+        response = super().update(request, *args, **kwargs)
+        instance = self.get_object()
+        for user in users:
+            instance.users.add(User.objects.get(id=user))
+        return response
